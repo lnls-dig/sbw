3fd771773e8bbb13153bd69658b9e153918c335b
Daniel Tavares <danielot@gmail.com>
2018-03-19 20:17:43 -0300

    downconv.m: make it work for arbitraty number of data channels

--- dsp_study/downconv.m
+++ dsp_study/downconv.m
@@ -3,7 +3,7 @@ function [ampl, iq] = downconv(x, f, fir_coeff)
 t = (0:size(x,1)-1)';
 nco = exp(-1j*(2*pi*f*t + pi/4));
 
-iq = x.*repmat(nco, 1, 4);
+iq = x.*repmat(nco, 1, size(x,2));
 iq = filter(fir_coeff, 1, iq);
 
 ampl = abs(iq);
