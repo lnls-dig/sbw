80fbb51006124c1d1ba6ae82584a39cf5cfb3afd
Daniel Tavares <danielot@gmail.com>
2018-03-19 23:43:58 -0300

    gensignals.m: change on variable names

--- dsp_study/gensignals.m
+++ dsp_study/gensignals.m
@@ -10,11 +10,11 @@ A = V*(1+bcd_factor*0.5*sin(2*pi*1/npts*t_samples));
 
 % BPM modulation signals
 xy_offset = repmat(beampos_offset, npts, 1);
-xy = builddrift(beampos_drift, npts, 2*1/10/382) + xy_offset;
+xy_ideal = builddrift(beampos_drift, npts, 2*1/10/382) + xy_offset;
 
 % Ideal amplitude signals
-pctx = 1 + xy(:,1)/Kx;
-pcty = 1 + xy(:,2)/Ky;
+pctx = 1 + xy_ideal(:,1)/Kx;
+pcty = 1 + xy_ideal(:,2)/Ky;
 
 to = A.*pctx.*pcty;
 ti = A.*pcty./pctx;
@@ -24,7 +24,7 @@ bo = A.*pctx./pcty;
 ampl = [to ti bi bo];
 
 % BPM carrier signals
-ampl_cw = ampl.*repmat(sin(2*pi*nif/nadc*t_samples), 1, 4);
+ampl_if = ampl.*repmat(sin(2*pi*nif/nadc*t_samples), 1, 4);
 
 % Channels drift
 chdrift = (1+builddrift(repmat(chdrift_level, 1, 4), npts, 0.001));
