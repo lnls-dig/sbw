bb76c8c204ea694a46abf200c15c013f4e988814
Daniel Tavares <daniel.tavares@lnls.br>
2019-04-24 17:11:33 -0300

    dsp_study/params.m: update parameters

--- dsp_study/params.m
+++ dsp_study/params.m
@@ -14,20 +14,20 @@ Ky = Kx;
 beampos_drift = [200e-9 200e-9];    % Beam position peak-to-peak variation [m]
 
 % Beam position offset [m]
-%beampos_offset = [0.5e-3 -0.25e-3]; % Centered beam
-beampos_offset = [0 0];             % Off-centered beam [m]
+beampos_offset = [0.5e-3 -0.25e-3]; % Off-centered beam [m]
+%beampos_offset = [0 0];             % Centered beam
 
 chdrift_level = 1e-3;               % Channel gain drift peak-to-peak [ratio]
 
 % Signal amplitude variation [ratio]
 bcd_factor = 0;
-%bcd_factor = 0.3;                   % Long-range BCD
-%bcd_factor = 0.02;                  % Short-range BCD
+%bcd_factor = 0.3;                   % Long-range BCD (decay mode)
+%bcd_factor = 0.02;                  % Short-range BCD (top-up mode)
 
 % Signal amplitude [V]
-% V = 0.02236;                        % -20 dBm @ 50 ohm (0 dBm on RFFE output)
-V = 0.03159;                        % -17 dBm @ 50 ohm (+3 dBm on RFFE output)
-% V = 0.04462;                        % -14 dBm @ 50 ohm (+6 dBm on RFFE output)
+%V = 0.02236;                        % -20 dBm @ 50 ohm (0 dBm @ RFFE output)
+%V = 0.03159;                        % -17 dBm @ 50 ohm (+3 dBm @ RFFE output)
+V = 0.04462;                        % -14 dBm @ 50 ohm (+6 dBm @ RFFE output)
 
 % RFFE non-linearity polynomial
 rffe_poly = [-1.152 0 1 0]*10;      % TAMP-72LN amplifier (only last stage taken into account for simplicity) - P1dB @ output = 20 dBm 
